d573c435a7326dc1de6b5582980bf0b498287137
Mathias Olesen <math09s0@stud.kea.dk>
2024-05-04 23:11:04 +0200

    frontpage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,33 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import Menu from "./components/menu";
 
 
 export default function Home() {
   return (
     <>
+    
+      <Menu className={styles.menu} />
+
+    <main>
     <Image className={styles.splash} src="/splash.jpg" alt="My Image" layout="fill"
   objectFit="cover"
     />
-    <Menu className={styles.menu} />
+    <section className={styles.container_1_1}>
+      <button>hello</button>
+      <h1>EACH PIECE IS HANDPAINTED</h1>
+      <p>hello Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    </section>
+    <div class="mat_content">
+          
+          <div class="scrolling_wrapper">
+            <div class="card">
+              
+            </div>
+          </div>
+          
+        </div>
+    </main>
     
     </>
   );
