c7afe685855508ec91101359f96d5661faa250cc
Mathias Olesen <math09s0@stud.kea.dk>
2024-05-31 02:13:19 +0200

    overflow x test

--- src/app/lib/datocms.jsx
+++ src/app/lib/datocms.jsx
@@ -16,19 +16,3 @@ export const performRequest = async ({ query, variables = {}, includeDrafts = fa
   
   return responseBody;
 }
-/*
-
-const DATOCMS_API_URL = 'https://graphql.datocms.com/';
-const NEXT_PUBLIC_DATOCMS_API_TOKEN = process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN;
-
-const client = new GraphQLClient(DATOCMS_API_URL, {
-  headers: {
-    authorization: `Bearer ${NEXT_PUBLIC_DATOCMS_API_TOKEN}`,
-  },
-});
-
-export const fetchDatoCMS = async (query, variables) => {
-  return client.request(query, variables);
-};
-
-*/
