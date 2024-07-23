ad4e5625a9997b545ca44d7b94a598dc5ac4ada8
Mathias Olesen <math09s0@stud.kea.dk>
2024-07-23 12:00:32 +0200

    .back button animation test mobil version

--- src/app/components/product_detail.jsx
+++ src/app/components/product_detail.jsx
@@ -97,10 +97,10 @@ const ProductDetail = ({ product }) => {
               <div className={styles.back_to_shop}>
                 <Link className={styles.shop_link} href='/pages/shop'>
                 <button className={styles.animated_button}>
-                <svg xmlns="http://www.w3.org/2000/svg" className={styles.arr_2} width="24" height="24" viewBox="0 0 24 24" fill="none"  stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" className={styles.arr_2} width="34" height="34" viewBox="0 0 24 24" fill="none"  stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg>
   <span className={styles.text}>back to shop</span>
   <span className={styles.circle}></span>
-  <svg xmlns="http://www.w3.org/2000/svg" className={styles.arr_1} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" className={styles.arr_1} width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg>
 </button>
                 </Link>
               </div>
